2d6fd52a98d9aa346fd6ebcb6b83a8999779c905
Wonseok Shin <wsshin97@gmail.com>
2016-11-18 17:40:38 -0500

    Remove the REPL's effect on running @time in test/time_volfrac.jl

--- test/time_volfrac.jl
+++ test/time_volfrac.jl
@@ -7,10 +7,14 @@ boxes = [sort(randn(3,2),2) for i = 1:nbox]
 r₀s = map(box->mean(box,2)[:,1], boxes)
 nouts = [randn(3) for i = 1:nbox]
 
-volfrac(boxes[1], nouts[1], r₀s[1])  # execute this to initiate compilation
+volfrac(boxes[1], nouts[1], r₀s[1])  # to force compilation
 
-@time begin
-    for i = 1:nbox
-        volfrac(boxes[i], nouts[i], r₀s[i])
+function benchmark()
+    @time begin
+        for i = 1:nbox
+            volfrac(boxes[i], nouts[i], r₀s[i])
+        end
     end
 end
+
+benchmark()  # run @time inside function to eliminate REPL effect
